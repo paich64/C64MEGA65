8e4f9e14ffab6d8f4808738ec5ef950ee9a251be
Michael Finn Jørgensen <mfj@weibel.dk>
2023-05-09 12:29:33 +0200

    Refactor sw_cartridge_wrapper

--- CORE/vhdl/sw_cartridge_wrapper.vhd
+++ CORE/vhdl/sw_cartridge_wrapper.vhd
@@ -62,37 +62,6 @@ architecture synthesis of sw_cartridge_wrapper is
 
    constant C_CACHE_SIZE : natural := 3;
 
-   constant C_ERROR_STRING_LENGTH : integer := 21;
-   type string_vector is array (natural range <>) of string(1 to C_ERROR_STRING_LENGTH);
-   constant C_ERROR_STRINGS : string_vector(0 to 7) := (
-     "OK                 \n",
-     "Missing CRT header \n",
-     "Missing CHIP header\n",
-     "Wrong CRT header   \n",
-     "Wrong CHIP header  \n",
-     "Truncated CHIP     \n",
-     "OK                 \n",
-     "OK                 \n");
-
-
-   -- Status reporting from the QNICE
-   constant C_CRT_ST_IDLE         : std_logic_vector(15 downto 0) := X"0000";
-   constant C_CRT_ST_LDNG         : std_logic_vector(15 downto 0) := X"0001";
-   constant C_CRT_ST_ERR          : std_logic_vector(15 downto 0) := X"0002";
-   constant C_CRT_ST_OK           : std_logic_vector(15 downto 0) := X"0003";
-
-   constant C_CRT_CASREG          : unsigned(15 downto 0) := X"FFFF";
-   constant C_CRT_STATUS          : unsigned(11 downto 0) := X"000";
-   constant C_CRT_FS_LO           : unsigned(11 downto 0) := X"001";
-   constant C_CRT_FS_HI           : unsigned(11 downto 0) := X"002";
-   constant C_CRT_PARSEST         : unsigned(11 downto 0) := X"010";
-   constant C_CRT_PARSEE1         : unsigned(11 downto 0) := X"011";
-   constant C_CRT_ADDR_LO         : unsigned(11 downto 0) := X"012";
-   constant C_CRT_ADDR_HI         : unsigned(11 downto 0) := X"013";
-   constant C_CRT_ERR_START       : unsigned(11 downto 0) := X"100";
-   constant C_CRT_ERR_END         : unsigned(11 downto 0) := X"1FF";
-
-
    -- Request and response
    signal qnice_req_status        : std_logic_vector(15 downto 0);
    signal qnice_req_length        : std_logic_vector(22 downto 0);
@@ -100,13 +69,6 @@ architecture synthesis of sw_cartridge_wrapper is
    signal qnice_resp_status       : std_logic_vector( 3 downto 0);
    signal qnice_resp_error        : std_logic_vector( 3 downto 0);
    signal qnice_resp_address      : std_logic_vector(22 downto 0);
-   signal qnice_stat_data         : std_logic_vector( 7 downto 0);
-
-   signal qnice_hr_ce             : std_logic;
-   signal qnice_hr_addr           : std_logic_vector(31 downto 0);
-   signal qnice_hr_wait           : std_logic;
-   signal qnice_hr_data           : std_logic_vector(15 downto 0);
-   signal qnice_hr_byteenable     : std_logic_vector( 1 downto 0);
 
    signal qnice_avm_write         : std_logic;
    signal qnice_avm_read          : std_logic;
@@ -222,135 +184,38 @@ architecture synthesis of sw_cartridge_wrapper is
 begin
 
    ---------------------------------------------------
-   -- Reset core when CRT file is successfully parsed
+   -- Handle QNICE interface (control and status)
    ---------------------------------------------------
 
-   process (main_clk_i)
-      constant C_STAT_READY : std_logic_vector(3 downto 0) := "0010"; -- Successfully parsed CRT file
-   begin
-      if rising_edge(main_clk_i) then
-         main_resp_status_d <= main_resp_status;
-         main_reset_core    <= main_reset_core(64 downto 0) & '0';
-         if main_resp_status = C_STAT_READY and main_resp_status_d /= C_STAT_READY then
-            main_reset_core <= (others => '1');
-         end if;
-         -- Stay in reset until cache is ready
-         if main_reset_core(65) = '1' and main_bank_wait_o = '1' then
-            main_reset_core <= (others => '1');
-         end if;
-         main_reset_core_o <= main_reset_core(65);
-      end if;
-   end process;
-
-   ----------------------------------------
-   -- Decode information from and to QNICE
-   ----------------------------------------
-
-   qnice_req_valid <= '1' when qnice_req_status = C_CRT_ST_OK else '0';
-
-   process (qnice_clk_i)
-   begin
-      if falling_edge(qnice_clk_i) then
-         if qnice_ce_i = '1' and
-            qnice_we_i = '1' and
-            unsigned(qnice_addr_i(27 downto 12)) = C_CRT_CASREG
-         then
-            case unsigned(qnice_addr_i(11 downto 0)) is
-               when C_CRT_STATUS => qnice_req_status                <= qnice_data_i;
-               when C_CRT_FS_LO  => qnice_req_length(15 downto  0)  <= qnice_data_i;
-               when C_CRT_FS_HI  => qnice_req_length(22 downto 16)  <= qnice_data_i(6 downto 0);
-               when others => null;
-            end case;
-         end if;
-
-         if qnice_rst_i = '1' then
-            qnice_req_status  <= (others => '0');
-            qnice_req_length  <= (others => '0');
-         end if;
-      end if;
-   end process;
-
-
-   -----------------------------------------
-   -- Generate error status string to QNICE
-   -----------------------------------------
-
-   process (all)
-      variable error_index_v : natural range 0 to 7;
-      variable char_index_v  : natural range 1 to 32;
-      variable char_v        : character;
-   begin
-      error_index_v := to_integer(unsigned(qnice_resp_error(2 downto 0)));
-      char_index_v  := to_integer(unsigned(qnice_addr_i(4 downto 0))) + 1;
-      if char_index_v <= C_ERROR_STRING_LENGTH then
-         char_v := C_ERROR_STRINGS(error_index_v)(char_index_v);
-         qnice_stat_data <= std_logic_vector(to_unsigned(character'pos(char_v), 8));
-      else
-         qnice_stat_data <= X"00"; -- zero-terminated strings
-      end if;
-   end process;
-
-   process (all)
-   begin
-      qnice_data_o <= x"0000"; -- By default read back zeros.
-      qnice_wait_o <= '0';
-
-      if qnice_ce_i = '1' and
-         qnice_we_i = '0' and
-         unsigned(qnice_addr_i(27 downto 12)) = C_CRT_CASREG
-      then
-         case to_integer(unsigned(qnice_addr_i(11 downto 0))) is
-            when to_integer(C_CRT_STATUS)  => qnice_data_o <= qnice_req_status;
-            when to_integer(C_CRT_FS_LO)   => qnice_data_o <= qnice_req_length(15 downto  0);
-            when to_integer(C_CRT_FS_HI)   => qnice_data_o(6 downto 0) <= qnice_req_length(22 downto 16);
-            when to_integer(C_CRT_PARSEST) => qnice_data_o <= X"000" & qnice_resp_status;
-            when to_integer(C_CRT_PARSEE1) => qnice_data_o <= X"000" & qnice_resp_error;
-            when to_integer(C_CRT_ADDR_LO) => qnice_data_o <= qnice_resp_address(15 downto 0);
-            when to_integer(C_CRT_ADDR_HI) => qnice_data_o <= "000000000" & qnice_resp_address(22 downto 16);
-            when to_integer(C_CRT_ERR_START)
-              to to_integer(C_CRT_ERR_END) => qnice_data_o <= X"00" & qnice_stat_data;
-            when others => null;
-         end case;
-      end if;
-
-      if qnice_ce_i = '1' and unsigned(qnice_addr_i(27 downto 12)) /= C_CRT_CASREG then
-         qnice_wait_o <= qnice_hr_wait;
-         if qnice_addr_i(0) = '1' then
-            qnice_data_o <= X"00" & qnice_hr_data(15 downto 8);
-         else
-            qnice_data_o <= X"00" & qnice_hr_data(7 downto 0);
-         end if;
-      end if;
-   end process;
-
-   qnice_hr_ce <= qnice_ce_i when unsigned(qnice_addr_i(27 downto 12)) /= C_CRT_CASREG
-             else '0';
-   qnice_hr_addr <= std_logic_vector(("00000" & unsigned(qnice_addr_i(27 downto 1))) +
-                                     ("0000000000" & unsigned(G_BASE_ADDRESS)));
-   qnice_hr_byteenable <= "10" when qnice_addr_i(0) = '1'
-                     else "01";
-
-   i_qnice2hyperram : entity work.qnice2hyperram
+   i_sw_cartridge_csr : entity work.sw_cartridge_csr
+      generic map (
+         G_BASE_ADDRESS => G_BASE_ADDRESS
+      )
       port map (
-         clk_i                 => qnice_clk_i,
-         rst_i                 => qnice_rst_i,
-         s_qnice_wait_o        => qnice_hr_wait,
-         s_qnice_address_i     => qnice_hr_addr,
-         s_qnice_cs_i          => qnice_hr_ce,
-         s_qnice_write_i       => qnice_we_i,
-         s_qnice_writedata_i   => qnice_data_i(7 downto 0) & qnice_data_i(7 downto 0),
-         s_qnice_byteenable_i  => qnice_hr_byteenable,
-         s_qnice_readdata_o    => qnice_hr_data,
-         m_avm_write_o         => qnice_avm_write,
-         m_avm_read_o          => qnice_avm_read,
-         m_avm_address_o       => qnice_avm_address,
-         m_avm_writedata_o     => qnice_avm_writedata,
-         m_avm_byteenable_o    => qnice_avm_byteenable,
-         m_avm_burstcount_o    => qnice_avm_burstcount,
-         m_avm_readdata_i      => qnice_avm_readdata,
-         m_avm_readdatavalid_i => qnice_avm_readdatavalid,
-         m_avm_waitrequest_i   => qnice_avm_waitrequest
-      ); -- i_qnice2hyperram
+         qnice_clk_i               => qnice_clk_i,
+         qnice_rst_i               => qnice_rst_i,
+         qnice_addr_i              => qnice_addr_i,
+         qnice_data_i              => qnice_data_i,
+         qnice_ce_i                => qnice_ce_i,
+         qnice_we_i                => qnice_we_i,
+         qnice_data_o              => qnice_data_o,
+         qnice_wait_o              => qnice_wait_o,
+         qnice_req_status_o        => qnice_req_status,
+         qnice_req_length_o        => qnice_req_length,
+         qnice_req_valid_o         => qnice_req_valid,
+         qnice_resp_status_i       => qnice_resp_status,
+         qnice_resp_error_i        => qnice_resp_error,
+         qnice_resp_address_i      => qnice_resp_address,
+         qnice_avm_write_o         => qnice_avm_write,
+         qnice_avm_read_o          => qnice_avm_read,
+         qnice_avm_address_o       => qnice_avm_address,
+         qnice_avm_writedata_o     => qnice_avm_writedata,
+         qnice_avm_byteenable_o    => qnice_avm_byteenable,
+         qnice_avm_burstcount_o    => qnice_avm_burstcount,
+         qnice_avm_readdata_i      => qnice_avm_readdata,
+         qnice_avm_readdatavalid_i => qnice_avm_readdatavalid,
+         qnice_avm_waitrequest_i   => qnice_avm_waitrequest
+      ); -- i_sw_cartridge_csr
 
 
    --------------------------------------------
@@ -405,42 +270,6 @@ begin
 
 
    --------------------------------------------
-   -- Clock Domain Crossing: HyperRAM -> QNICE
-   --------------------------------------------
-
-   i_cdc_hr2qnice : entity work.cdc_stable
-      generic map (
-         G_DATA_SIZE => 31
-      )
-      port map (
-         src_clk_i               => hr_clk_i,
-         src_data_i( 3 downto 0) => hr_resp_status,
-         src_data_i( 7 downto 4) => hr_resp_error,
-         src_data_i(30 downto 8) => hr_resp_address,
-         dst_clk_i               => qnice_clk_i,
-         dst_data_o( 3 downto 0) => qnice_resp_status,
-         dst_data_o( 7 downto 4) => qnice_resp_error,
-         dst_data_o(30 downto 8) => qnice_resp_address
-      ); -- i_cdc_hr2qnice
-
-
-   --------------------------------------------
-   -- Clock Domain Crossing: HyperRAM -> CORE
-   --------------------------------------------
-
-   i_cdc_hr2main : entity work.cdc_stable
-      generic map (
-         G_DATA_SIZE => 4
-      )
-      port map (
-         src_clk_i              => hr_clk_i,
-         src_data_i(3 downto 0) => hr_resp_status,
-         dst_clk_i              => main_clk_i,
-         dst_data_o(3 downto 0) => main_resp_status
-      ); -- i_cdc_hr2main
-
-
-   --------------------------------------------
    -- Clock Domain Crossing: CORE -> HyperRAM
    --------------------------------------------
 
@@ -460,7 +289,7 @@ begin
 
    -------------------------------------------------------------
    -- Instantiate CRT loader and parser
-   -- This module runs entire within the HyperRAM clock domain.
+   -- This module runs entirely within the HyperRAM clock domain.
    -------------------------------------------------------------
 
    i_crt_loader : entity work.crt_loader
@@ -508,6 +337,11 @@ begin
          bram_hi_q_i         => (others => '0')
       ); -- i_crt_loader
 
+
+   --------------------------------------------
+   -- Arbiter for HypeRAM access
+   --------------------------------------------
+
    i_avm_arbit : entity work.avm_arbit
       generic map (
          G_ADDRESS_SIZE  => 32,
@@ -544,6 +378,26 @@ begin
          m_avm_readdatavalid_i  => hr_readdatavalid_i,
          m_avm_waitrequest_i    => hr_waitrequest_i
       ); -- i_avm_arbit
+
+
+   --------------------------------------------
+   -- Clock Domain Crossing: HyperRAM -> QNICE
+   --------------------------------------------
+
+   i_cdc_hr2qnice : entity work.cdc_stable
+      generic map (
+         G_DATA_SIZE => 31
+      )
+      port map (
+         src_clk_i               => hr_clk_i,
+         src_data_i( 3 downto 0) => hr_resp_status,
+         src_data_i( 7 downto 4) => hr_resp_error,
+         src_data_i(30 downto 8) => hr_resp_address,
+         dst_clk_i               => qnice_clk_i,
+         dst_data_o( 3 downto 0) => qnice_resp_status,
+         dst_data_o( 7 downto 4) => qnice_resp_error,
+         dst_data_o(30 downto 8) => qnice_resp_address
+      ); -- i_cdc_hr2qnice
 
 
    --------------------------------------------
@@ -597,6 +451,39 @@ begin
        dst_data_o(56+2*C_CACHE_SIZE downto 57+C_CACHE_SIZE) => main_cache_addr_hi
      ); -- i_cdc_stable
 
+   i_cdc_hr2main : entity work.cdc_stable
+      generic map (
+         G_DATA_SIZE => 4
+      )
+      port map (
+         src_clk_i              => hr_clk_i,
+         src_data_i(3 downto 0) => hr_resp_status,
+         dst_clk_i              => main_clk_i,
+         dst_data_o(3 downto 0) => main_resp_status
+      ); -- i_cdc_hr2main
+
+
+   ---------------------------------------------------
+   -- Reset core when CRT file is successfully parsed
+   ---------------------------------------------------
+
+   process (main_clk_i)
+      constant C_STAT_READY : std_logic_vector(3 downto 0) := "0010"; -- Successfully parsed CRT file
+   begin
+      if rising_edge(main_clk_i) then
+         main_resp_status_d <= main_resp_status;
+         main_reset_core    <= main_reset_core(64 downto 0) & '0';
+         if main_resp_status = C_STAT_READY and main_resp_status_d /= C_STAT_READY then
+            main_reset_core <= (others => '1');
+         end if;
+         -- Stay in reset until cache is ready
+         if main_reset_core(65) = '1' and main_bank_wait_o = '1' then
+            main_reset_core <= (others => '1');
+         end if;
+         main_reset_core_o <= main_reset_core(65);
+      end if;
+   end process;
+
 
    -------------------------------------------------------------
    -- Instantiate bank cache memory
@@ -645,6 +532,11 @@ begin
          wren_b     => hr_bram_hi_wren,
          q_b        => open
       ); -- crt_lo_ram
+
+
+   -------------------------------------------------------------
+   -- Instantiate I/O memory
+   -------------------------------------------------------------
 
    ioe_ram : entity work.dualport_2clk_ram
       generic map (
